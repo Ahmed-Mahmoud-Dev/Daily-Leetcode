d71d8bf87267b2ebd3848eb1afb039aafdac1b08
Ahmed-Mahmoud-Dev <ahmedmrmdn@gmail.com>
2025-07-03 00:40:11 +0300

    Time: 29 ms (5.25%) | Memory: 35.3 MB (58.01%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cs
@@ -0,0 +1,15 @@
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        int reverse = 0, tmp = x;
+
+        while (tmp > 0)
+        {
+            reverse = reverse * 10 + tmp % 10;
+            tmp /= 10;
+        }
+        Console.WriteLine(reverse);
+        return (x == reverse);
+    }
+}
